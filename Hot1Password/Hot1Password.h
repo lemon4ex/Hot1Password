919cafd1c0bf7973878b8a932490997be28a4c5d
lemon4ex <netchen@vip.qq.com>
2019-04-03 00:44:52 +0800

    Initial Commit

---
+++ Hot1Password/Hot1Password.h
@@ -0,0 +1,9 @@
+//
+//  Hot1Password.h
+//  Hot1Password
+//
+//  Created by h4ck on 2019/4/3.
+//  Copyright (c) 2019年 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Hot1Password/Hot1Password.m
@@ -0,0 +1,15 @@
+//
+//  Hot1Password.m
+//  Hot1Password
+//
+//  Created by h4ck on 2019/4/3.
+//  Copyright (c) 2019年 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "Hot1Password.h"
+#import <CaptainHook/CaptainHook.h>
+
+CHConstructor // code block that runs immediately upon load
+{
+    NSLog(@"++++++++ Hot1Password loaded ++++++++");
+}
